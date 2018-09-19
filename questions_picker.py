cb8a4029554f1e16dee9ac49d940825706b685c0
Carlos Rivera <crivera1@underarmour.com>
2018-09-19 11:48:21 -0500

    Parse CSV into taxonomy dictionary

--- questions_picker.py
+++ questions_picker.py
@@ -1,4 +1,36 @@
+import csv
 import sys
+
+def load_taxonomy_from_csv(filename):
+  strands = {}
+  with open(filename, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      strand_id = row['strand_id']
+      strand_name = row['strand_name']
+      standard_id = row['standard_id']
+      standard_name = row['standard_name']
+      question_id = row['question_id']
+      difficulty = row['difficulty']
+
+      if strand_id not in strands:
+        strands[strand_id] = {
+          'name': strand_name,
+          'standards': {}
+        }
+
+      if standard_id not in strands[strand_id]['standards']:
+        strands[strand_id]['standards'][standard_id] = {
+          'name': standard_name,
+          'questions': {}
+        }
+
+      strands[strand_id]['standards'][standard_id]['questions'][question_id] = {
+        'difficulty': float(difficulty)
+      }
+
+  return strands
+
 
 def pick_questions(count):
   # TODO
@@ -9,7 +41,32 @@ if __name__ == '__main__':
     print("Usage: python3 questions_picker.py <NUMBER OF QUESTIONS>")
     sys.exit()
 
+  taxonomy = load_taxonomy_from_csv('questions.csv')
+
   num_questions = int(sys.argv[1])
   questions_list = pick_questions(num_questions)
   print(','.join(questions_list))
 
+
+"""
+{'1': {'name': 'Nouns',
+       'standards': {'1': {'name': 'Common Nouns',
+                           'questions': {'1': {'difficulty': 0.7},
+                                         '2': {'difficulty': 0.6}}},
+                     '2': {'name': 'Abstract Nouns',
+                           'questions': {'3': {'difficulty': 0.8}}},
+                     '3': {'name': 'Proper Nouns',
+                           'questions': {'4': {'difficulty': 0.2},
+                                         '5': {'difficulty': 0.5},
+                                         '6': {'difficulty': 0.4}}}}},
+ '2': {'name': 'Verbs',
+       'standards': {'4': {'name': 'Action Verbs',
+                           'questions': {'7': {'difficulty': 0.9},
+                                         '8': {'difficulty': 0.1}}},
+                     '5': {'name': 'Transitive Verbs',
+                           'questions': {'10': {'difficulty': 0.6},
+                                         '11': {'difficulty': 0.4},
+                                         '9': {'difficulty': 0.3}}},
+                     '6': {'name': 'Reflexive Verbs',
+                           'questions': {'12': {'difficulty': 0.2}}}}}}
+"""
